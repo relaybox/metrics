e8cbe0f7f3628a0d3d3e45d5e628c5dfaadf1805
livecloud-labs <livecloud.store@gmail.com>
2024-08-19 12:14:14 +0100

    chore: point to redis replication group

--- src/module/worker.ts
+++ src/module/worker.ts
@@ -1,46 +1,37 @@
 import { Job, Worker } from 'bullmq';
 import { getLogger } from '../util/logger.util';
 import { JobName, router } from './router';
-import { createClient } from '../lib/redis';
+import { connectionOptionsIo, getRedisClient } from '../lib/redis';
 import { getPgPool } from '../lib/pg';
 
-const logger = getLogger('metrics');
-
 const QUEUE_NAME = 'metrics';
-const REDIS_HOST = process.env.REDIS_HOST!;
-const REDIS_PORT = Number(process.env.REDIS_PORT!);
 
-const connectionOpts = {
-  host: REDIS_HOST,
-  port: REDIS_PORT
-};
-
-const redisClient = createClient({
-  host: REDIS_HOST,
-  port: REDIS_PORT
-});
-
+const logger = getLogger(QUEUE_NAME);
 const pgPool = getPgPool();
+const redisClient = getRedisClient();
 
-async function handler({ name, data }: Job) {
-  try {
-    logger.info(`Processing ${name}`, { data });
-    await router(pgPool, redisClient, name as JobName, data);
-  } catch (err) {
-    logger.error(`Processing job (${name}) failed`, { err, data });
-    throw err;
-  }
+async function handler({ id, name, data }: Job) {
+  logger.info(`Processing job ${id} (${name})`, { data });
+  await router(pgPool, redisClient, name as JobName, data);
 }
 
 export async function startWorker() {
   await redisClient.connect();
 
   const worker = new Worker(QUEUE_NAME, handler, {
-    connection: connectionOpts,
+    connection: connectionOptionsIo,
     prefix: 'queue'
   });
 
   worker.on('failed', (job: Job<any, void, string> | undefined, err: Error, prev: string) => {
     logger.error(`Failed to process job ${job?.id}`, { err });
+  });
+
+  worker.on('ready', () => {
+    logger.info(`Session worker ready`);
+  });
+
+  worker.on('active', () => {
+    logger.info(`Session worker active`);
   });
 }
